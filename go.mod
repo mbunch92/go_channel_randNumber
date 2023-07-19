2048d21e8183f8a7eee3c19388f2fd68c8e31a20
michaelbunch <michael1134@hotmail.com>
2023-07-18 18:17:46 -0700

    commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mbunch92/go_channel_randNumber.git
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+
+	"github.com/mbunch92/go_channel_randNumber.git/helpers"
+)
+
+const numPool = 1000
+
+func CalculateValue(intChan chan int) {
+	randomNumber := helpers.RandomNumber(numPool)
+	intChan <- randomNumber
+}
+
+func main() {
+	intChan := make(chan int)
+	defer close(intChan)
+
+	go CalculateValue(intChan)
+
+	num := <-intChan
+	log.Println(num)
+}
